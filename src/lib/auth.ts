5f046220d7011f7cad18bd95d2af9496b0ce6ace
LHB-Projects <lhb.projects@gmail.com>
2025-07-31 16:41:00 -0400

    secret

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,6 +7,7 @@ const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
   session: { strategy: "jwt" },
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
       name: "Credentials",
